191da46d7622a23fbc0ec65eb21ba9bb77cee258
noorlan <kozuev@gmail.com>
2021-07-31 22:46:40 +0000

    ready to deploy

---
+++ sec_group/output.tf
@@ -0,0 +1,11 @@
+output "sec_group_id" {
+  value = aws_security_group.allow_tls.sec_group_id
+}
+
+output "region" {
+  value = "us-east-1"
+}
+
+output "attached vpc" {
+  value = aws_security_group.allow_tls.vpc_id
+}
