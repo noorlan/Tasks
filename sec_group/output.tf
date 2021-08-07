0bfde5404d983eaafc556c4eece9392dff204044
noorlan <kozuev@gmail.com>
2021-08-07 18:35:25 +0000

    security group

--- sec_group/output.tf
+++ sec_group/output.tf
@@ -1,11 +1,11 @@
 output "sec_group_id" {
-  value = aws_security_group.allow_tls.sec_group_id
+  value = aws_security_group.external_by_terraform.id
 }
 
-output "region" {
-  value = "us-east-1"
+output "owner_id" {
+  value = aws_security_group.external_by_terraform.owner_id
 }
 
-output "attached_vpc" {
-  value = aws_security_group.allow_tls.vpc_id
+output "arn" {
+  value = aws_security_group.external_by_terraform.arn
 }
--- sec_group/sec_group.tf
+++ sec_group/sec_group.tf
@@ -1,19 +1,38 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+resource "aws_security_group" "external_by_terraform" {
+  name        = "external_by_terraform"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 3306
-    protocol         = "tcp"
-    cidr_blocks      = [ "50.194.68.230/32",
-                         "50.194.68.230/32" ]
-
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["50.194.68.230/32"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["50.194.68.231/32"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
-    from_port        = 80
-    to_port          = 443
+    from_port        = 0
+    to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
